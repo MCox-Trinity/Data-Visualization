65cccf093af3c8deeb8aa0d2b2cf9bf7fcb51103
Maddie Cox <mcox1@trinity.edu>
2020-10-28 12:50:10 -0500

    Class 10/28/2020

--- in_class/brushing/brushing.js
+++ in_class/brushing/brushing.js
@@ -1,5 +1,5 @@
 var margin = { top: 30, right: 20, bottom: 30, left: 50 },
-    width = 1000 - margin.left - margin.right,
+    width = 900 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
 
@@ -20,6 +20,7 @@ const svg = d3.select("body")
 
 const brush = d3.brushX()
     .extent([[margin.left, margin.top], [width-margin.right, height-margin.bottom]])
+    .on("start brush end", brushed);
 
 const circle = svg.append("g")
     .selectAll("circle")
@@ -28,8 +29,37 @@ const circle = svg.append("g")
         .attr("transform", d => `translate(${x(d)}, ${ry()})`)
         .attr("r", 5.5)
         .attr("fill-opacity", .4)
-        .attr("fill", "red");
+        .attr("fill", "purple");
 
 
 svg.append("g").call(xAxis);
 svg.append("g").call(brush);
+
+svg.append("g")
+    .call(brush)
+    .call(brush.move, [4, 6].map(x))
+    .call(g => g.select(".overlay")
+        .datum({type: "selection"})
+        .on("mousedown", brushClick));
+
+function brushed(event){
+    const selection = event.selection;
+    if(selection === null){
+        circle.attr("fill", "purple");
+    } else {
+        const [x0, x1] = selection.map(x.invert);
+        circle.attr("fill", d => x0 <= d && d <= x1 ? "lime" : "purple")
+    }
+}
+
+function brushClick(event){
+    const brushWidth = x(2) - x(0);
+    const [[clickLocation]] = d3.pointers(event);
+    const [x0, x1] = [clickLocation-brushWidth/2, clickLocation+brushWidth/2];
+    const [leftBound, rightBound] = x.range()
+    d3.select(this.parentNode)
+        .call(brush.move, 
+            x1 > rightBound ? [rightBound-brushWidth, rightBound] : 
+            x0 < leftBound ? [leftBound, leftBound+brushWidth] : 
+            [x0, x1]);
+}
